807880d48898de803df1d82f70e72dda3cf81b37
Aarthi S <aarthia776@gmail.com>
2026-03-16 19:15:39 +0530

    Update streamlit_app.py

--- streamlittask/streamlit_app.py
+++ streamlittask/streamlit_app.py
@@ -6,10 +6,6 @@ st.title("Streamlit Introduction App")
 
 st.markdown("### Basic Streamlit Demo")
 
-
-st.image("C:\Users\Administrator\Documents\Sourcesysintern\streamlittask.png", width=200)
-
-
 name = st.text_input("Enter your Name")
 
 
