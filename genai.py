adcdf2b7f6ee0fca577710c9c21a062a5285c30b
Aarthi S <aarthia776@gmail.com>
2026-03-26 13:12:03 +0530

    Update genai.py

--- genai.py
+++ genai.py
@@ -5,23 +5,17 @@ import google.generativeai as genai
 import os
 from dotenv import load_dotenv
 
-# Load .env file
 load_dotenv()
 
-# Get API key
 api_key = os.getenv("GEMINI_API_KEY")
 
-# Configure Gemini
 genai.configure(api_key=api_key)
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-# Sentiment model
 sentiment_model = pipeline("sentiment-analysis")
 
-# UI
 st.title("🤖 AI Chatbot + Sentiment Analysis")
 
-# Sentiment
 st.header("📊 Sentiment Analysis")
 text = st.text_area("Enter text")
 
@@ -30,7 +24,6 @@ if st.button("Analyze"):
         result = sentiment_model(text)
         st.write(result)
 
-# Chatbot
 st.header("💬 Chatbot")
 msg = st.text_input("Ask something")
 
